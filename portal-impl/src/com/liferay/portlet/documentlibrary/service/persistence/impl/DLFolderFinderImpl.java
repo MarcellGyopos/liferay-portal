619a0308a892d2f60c4b0a0d55f43c417f9949d4
Marcell Gyöpös <marcell.gyopos@liferay.com>
2019-12-11 17:00:02 +0100

    LPS-104763 Delete owner condition

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/persistence/impl/DLFolderFinderImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/persistence/impl/DLFolderFinderImpl.java
@@ -708,14 +708,6 @@ public class DLFolderFinderImpl
 			qPos.add(folderId);
 			qPos.add(groupId);
 			qPos.add(queryDefinition.getStatus());
-
-			if ((queryDefinition.getOwnerUserId() > 0) &&
-				queryDefinition.isIncludeOwner()) {
-
-				qPos.add(queryDefinition.getOwnerUserId());
-				qPos.add(WorkflowConstants.STATUS_IN_TRASH);
-			}
-
 			qPos.add(folderId);
 
 			if (mimeTypes != null) {
