5420747cecb2d3052b46bfec64b95c8703bdb276
Marcell Gyöpös <marcell.gyopos@liferay.com>
2021-01-25 15:33:02 +0100

    LPS-125750 Upgrade LayoutTable classNameId to 0 from null

--- portal-impl/src/com/liferay/portal/upgrade/v7_4_x/PortalUpgradeProcessRegistryImpl.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/PortalUpgradeProcessRegistryImpl.java
@@ -42,6 +42,8 @@ public class PortalUpgradeProcessRegistryImpl
 
 		upgradeProcesses.put(
 			new Version(10, 0, 0), new UpgradePortletPreferences());
+
+		upgradeProcesses.put(new Version(10, 0, 1), new UpgradeLayout());
 	}
 
 }
